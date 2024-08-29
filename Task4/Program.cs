48408756f2a2b7ae3bff3f9ab712e9cf41d3219d
Efremova Aleksandra <doctor.akab@gmail.com>
2024-08-29 17:35:21 +0300

    hwSeminar6 : Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Обратный порядок слов в строке
+
+void Main()
+{
+    string str = "one two three four five";
+    string newStr =  ReversString(str);
+    System.Console.WriteLine(str);
+    System.Console.WriteLine(newStr);
+}
+// метод реверсии
+string ReversString(string str)
+{
+    string[] array = str.Split(' ');
+    Array.Reverse(array);
+    return string.Join(", ", array);
+}
+
+
+
+
+
+Main();
