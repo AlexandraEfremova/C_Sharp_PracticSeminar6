237a25f1fd984f1c8b7ac4655aaa138b7e220795
Efremova Aleksandra <doctor.akab@gmail.com>
2024-08-29 15:09:11 +0300

    hwSeminar6 : Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+void Main()
+{
+    string str = PrintInt("Введите строку латинскими буквами и символами '\t' верхнего и нижнего регистров: ");
+    string strToLower = ToLowerString(str);
+    System.Console.WriteLine(strToLower);
+}
+// приведение в нижний регистр
+string ToLowerString(string str)
+{
+    return str = str!.ToLower();
+}
+
+// запрос строки от пользователя
+string PrintInt(string msg)
+{
+    System.Console.Write(msg);
+    return Console.ReadLine()!;
+} 
+Main();
